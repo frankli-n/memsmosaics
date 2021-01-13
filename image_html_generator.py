3cabe45c12087cd6da2ba849aee2eb373bea65f3
frankli-n <frankiehaubrich@gmail.com>
2021-01-12 16:24:41 -0800

    a

--- image_html_generator.py
+++ image_html_generator.py
@@ -4,9 +4,14 @@ for e in os.listdir("./images_organised_small/gallery_small"):
 	pass
 	# print "<a href='../images_organised_large/art_for_sale/%s' target=blank> <img src='../images_organised_small/art_for_sale/%s' class='all forsale'></a>"%(e,e)
 
-for e in os.listdir("./images_organised_small/gallery_small"):
-	# print "<a href='../images_organised_large/art_sold/%s' target=blank> <img src='../images_organised_small/art_sold/%s' class='all sold'></a>"%(e,e)
-	print "<img src='../images_organised_small/gallery_small/%s' class='all mosaics'>"%(e)
+for e in os.listdir("./images_organised_large/gallery_large"):
+    css_class = e.split('_')
+    print css_class
+    for x in css_class:
+      if x == 'all' or 'nature' or 'new' or 'jewellery' or 'people' or 'other':
+        print x
+    print "<a href='../images_organised_large/gallery_large/%s' target=blank> <img src='../images_organised_small/gallery_small/%s'"%(e,e)
+	# print "<img src='../images_organised_small/gallery_small/%s' class='all mosaics'>"%(e)
 
 '''
   <a class='gallery-link' href='../images/2boat.jpg'>
